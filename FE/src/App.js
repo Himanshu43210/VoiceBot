3fed8ee8a461f8417cf504efb070ca9c66972412
Himanshu Shukla <himanshu@techhelps.co.in>
2024-05-17 00:07:57 +0530

    Update App.js

--- FE/src/App.js
+++ FE/src/App.js
@@ -5,7 +5,7 @@ import VoiceChatBot from './VoiceChatBot';
 function App() {
   return (
     <>
-      <h1>Speech to Text App</h1>
+      <h1>Voice Assistant Application</h1>
       <VoiceChatBot />
     </>
   );
